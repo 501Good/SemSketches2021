a76e3f7479c46b206034447f977681b936216cf2
Kirill Milintsevich <milintsevich@gmail.com>
2021-04-05 18:50:58 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # SemSketches2021
 The solution for the Task 1 of the SemSketches competition for the Dialog 2021. (https://github.com/dialogue-evaluation/SemSketches)
+
+# Results
+| dataset  | score |
+|----------|-------|
+|dev       |0.104  |
+|manual dev|0.127  |
+
+# Requirements
+
+## Python
+- [sentence_transformers](https://github.com/UKPLab/sentence-transformers)
+- torch
+- numpy
+- tqdm
+
+# Instructions
+
+## Training
+1. Clone the [SemSketches repository](https://github.com/dialogue-evaluation/SemSketches) and copy the `data` folder;
+2. Download the [PyTorch Sentence RuBERT, Russian, cased by DeepPavlov](http://docs.deeppavlov.ai/en/master/features/models/bert.html) and unpack the `pytorch_model.bin` and `vocab.txt` files into `sentence_ru_cased_pipe_L-12_H-768_A-12_pt/0_Transformer/` folder;
+3. Follow the steps in the `SentenceTransformersSiamese.ipynb`.
+
+## Inference
+1. [Download the trained model](https://drive.google.com/file/d/1a3iViYBPmQy9xSgcIKRCfrJOPUNQrgju/view?usp=sharing) and unpack it into the main directory;
+2. Follow the steps in the `SentenceTransformersSiamese.ipynb`.
---
+++ modules/pooling.py
@@ -0,0 +1,66 @@
+from sentence_transformers.models import Pooling
+import torch
+import os
+import json
+
+class EntityPooling(Pooling):
+    def __init__(self, 
+                 entity_token_id: int, 
+                 word_embedding_dimension: int,
+                 pooling_mode_cls_token: bool = False,
+                 pooling_mode_max_tokens: bool = False,
+                 pooling_mode_mean_tokens: bool = True,
+                 pooling_mode_mean_sqrt_len_tokens: bool = False,
+                ):
+        super().__init__(word_embedding_dimension, 
+                         pooling_mode_cls_token,
+                         pooling_mode_max_tokens,
+                         pooling_mode_mean_tokens,
+                         pooling_mode_mean_sqrt_len_tokens,
+                        )
+        self.config_keys = ['entity_token_id', 'word_embedding_dimension',  'pooling_mode_cls_token', 'pooling_mode_mean_tokens', 'pooling_mode_max_tokens', 'pooling_mode_mean_sqrt_len_tokens']
+
+        self.entity_token_id = entity_token_id
+        
+    def forward(self, features):
+        token_embeddings = features['token_embeddings']
+        cls_token = features['cls_token_embeddings']
+        attention_mask = features['attention_mask']
+        entity_mask = torch.where(features['input_ids'] == self.entity_token_id, 1, 0)
+        
+        ## Pooling strategy
+        output_vectors = []
+        if self.pooling_mode_cls_token:
+            output_vectors.append(cls_token)
+        if self.pooling_mode_max_tokens:
+            input_mask_expanded = entity_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+            token_embeddings[input_mask_expanded == 0] = -1e9  # Set padding tokens to large negative value
+            max_over_time = torch.max(token_embeddings, 1)[0]
+            output_vectors.append(max_over_time)
+        if self.pooling_mode_mean_tokens or self.pooling_mode_mean_sqrt_len_tokens:
+            input_mask_expanded = entity_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+            sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
+
+            #If tokens are weighted (by WordWeights layer), feature 'token_weights_sum' will be present
+            if 'token_weights_sum' in features:
+                sum_mask = features['token_weights_sum'].unsqueeze(-1).expand(sum_embeddings.size())
+            else:
+                sum_mask = input_mask_expanded.sum(1)
+
+            sum_mask = torch.clamp(sum_mask, min=1e-9)
+
+            if self.pooling_mode_mean_tokens:
+                output_vectors.append(sum_embeddings / sum_mask)
+            if self.pooling_mode_mean_sqrt_len_tokens:
+                output_vectors.append(sum_embeddings / torch.sqrt(sum_mask))
+
+        output_vector = torch.cat(output_vectors, 1)
+        features.update({'sentence_embedding': output_vector})
+        return features
+    
+    @staticmethod
+    def load(input_path):
+        with open(os.path.join(input_path, 'config.json')) as fIn:
+            config = json.load(fIn)
+
+        return EntityPooling(**config)
